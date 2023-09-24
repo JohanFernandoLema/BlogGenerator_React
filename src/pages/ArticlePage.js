70dba705ec3b92c4ea4cbc1b5f245fd6a8d25e6d
JohanFernandoLema <johan.13fernando@gmail.com>
2023-09-24 14:41:43 -0400

    articles being displayed according to their title

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,6 +1,17 @@
 import { useParams } from 'react-router-dom'
+import articles from './article-content'
+
 const ArticlePage = () => {
   const { articleId } = useParams()
-  return <h1>You are about to read the article with the id: </h1>
+
+  const article = articles.find((article) => article.name === articleId)
+  return (
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph, index) => (
+        <p key={index}>{paragraph}</p>
+      ))}
+    </>
+  )
 }
 export default ArticlePage
