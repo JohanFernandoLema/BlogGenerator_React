02e1f233432c55cc8be094ed963107a1280db2f6
JohanFernandoLema <johan.13fernando@gmail.com>
2023-10-01 22:01:44 -0400

    proxy error fixed - server is getting connected towards front end

--- client/src/pages/ArticlePage.js
+++ client/src/pages/ArticlePage.js
@@ -1,10 +1,22 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import { useParams } from 'react-router-dom'
+
 import articles from './article-content'
 import NotFoundPage from './NotFoundPage'
 
 const ArticlePage = () => {
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] })
   const { articleId } = useParams()
 
+  useEffect(() => {
+    const newArticleInfo = async () => {
+      const res = await axios.get(`/api/articles/${articleId}`)
+      setArticleInfo(res.data)
+    }
+    // setArticleInfo({ upvotes: 5, comments: [] })
+    newArticleInfo()
+  }, [])
   const article = articles.find((article) => article.name === articleId)
 
   if (!article) {
@@ -17,6 +29,9 @@ const ArticlePage = () => {
   return (
     <>
       <h1>{article.title}</h1>
+      <h6>
+        This {article.name} has {articleInfo.upvotes} upvote(s)
+      </h6>
       {article.content.map((paragraph, index) => (
         <p key={index}>{paragraph}</p>
       ))}
