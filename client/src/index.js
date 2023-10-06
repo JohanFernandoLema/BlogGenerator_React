7d7e79a0e2ed1c0ae09e8967a5fbf6f1a89346e3
JohanFernandoLema <johan.13fernando@gmail.com>
2023-10-06 16:34:21 -0400

    keys added to the project in order to keep track of users

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,24 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDq9faIyZLWw1FuUNGuenKzPpJo3hUQF6M',
+  authDomain: 'react-blog-ae75e.firebaseapp.com',
+  projectId: 'react-blog-ae75e',
+  storageBucket: 'react-blog-ae75e.appspot.com',
+  messagingSenderId: '85680699538',
+  appId: '1:85680699538:web:1e119f2b08f506dd230e4b',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
