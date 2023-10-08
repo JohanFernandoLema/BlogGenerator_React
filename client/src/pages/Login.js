4d9a7e20997b272560d9248e908ccb8ca06d0911
JohanFernandoLema <johan.13fernando@gmail.com>
2023-10-07 21:09:36 -0400

    user can input their credential in the login page - tehy will be able to login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,51 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  const navigate = useNavigate()
+  const logIn = async () => {
+    try {
+      await signInWithEmailAndPassword(getAuth(), email, password)
+      navigate('/articles')
+    } catch (error) {
+      setError(error.message)
+    }
+  }
   return (
     <>
+      {error && <p className="error">{error}</p>}
       <div>
-        Input your credentials down here
+        <label htmlFor="e_mail">
+          E-mail
+          <input
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value)
+            }}
+            placeholder="john@example.com"
+            type="email"
+            name="userEmail"
+            id="userEmail"
+          />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
+            placeholder="Password"
+            type="password"
+            name="userPassword"
+            id="userPassword"
+          />
+        </label>
+        <button onClick={logIn}>Sing In</button>
         <h4>Do not have an account?</h4>
         <Link to={'/register'}>
           <button>Register</button>
